047b992fddc143394a65adc7d72837379a45b14d
Andrey Assaul <andrey.assaul@gmail.com>
2015-08-04 02:05:57 +0300

    * added design for details view and population of basic data

--- app/src/main/java/com/nanodegree/assaul/popularmoviesapp/activity/MovieDetailsActivity.java
+++ app/src/main/java/com/nanodegree/assaul/popularmoviesapp/activity/MovieDetailsActivity.java
@@ -1,7 +1,6 @@
 package com.nanodegree.assaul.popularmoviesapp.activity;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -10,10 +9,16 @@ import com.nanodegree.assaul.popularmoviesapp.R;
 import com.nanodegree.assaul.popularmoviesapp.model.MovieVO;
 import com.squareup.picasso.Picasso;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 /**
  * Created by Andrey Assaul on 06.07.2015.
  */
 public class MovieDetailsActivity extends AppCompatActivity {
+    private final static String DATE_FORMAT_PATTERN = "yyyy";
+    private final static DateFormat DATE_FORMAT = new SimpleDateFormat(DATE_FORMAT_PATTERN, Locale.getDefault());
 
     public static final String MOVIE_DATA = "MOVIE_DATA";
 
@@ -35,12 +40,14 @@ public class MovieDetailsActivity extends AppCompatActivity {
 
     private void initContent() {
         Picasso picasso = Picasso.with(this);
-        TextView movieNameTextView = (TextView)findViewById(R.id.movieNemaTextView);
-        movieNameTextView.setText(movie.getTitle());
         picasso.load(getBaseUrl()+movie.getPosterPath()).into((ImageView)findViewById(R.id.movieImageView));
+        ((TextView)findViewById(R.id.movieNameTextView)).setText(movie.getTitle());
+        ((TextView)findViewById(R.id.movieSynopsisTextView)).setText(movie.getOverview());
+        ((TextView)findViewById(R.id.movieVoteTextView)).setText(String.valueOf(movie.getVoteAverage()) + "/10");
+        ((TextView)findViewById(R.id.movieYaerTextView)).setText(DATE_FORMAT.format(movie.getReleaseDate()));
     }
 
     private String getBaseUrl(){
-        return getString(R.string.themoviedb_image_base_url)+"154/";
+        return getString(R.string.themoviedb_image_base_url)+"185/";
     }
 }
